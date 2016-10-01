064348e8abe152b7d49e17e76e4ccc3d20d425d1
Anija <anija.nair@yahoo.com>
2016-10-01 16:02:02 -0400

    bound task events to buttons

--- Todo App/js/app.js
+++ Todo App/js/app.js
@@ -7,31 +7,40 @@ var completedTasksHolder =document.getElementById("completed-tasks");
 
 //add new task
 var addTask = function(){
-console.log("add text..");
+console.log("add texts..");
 }
 
 //edit existing task
 var editTask = function(){
-
+console.log( "edit aaa");
 }
 
 //delete an existing task
 var deleteTask = function(){
+  console.log("delete ... text..");
 
 }
 
 //mark a task as completed
-var taskCompleted = function(){
+var taskCompleted = function(){console.log("finish text..");
 
 }
 
 //mark a task as incomplete
 var taskIncomplete = function(){
+  console.log("incomplete text..");
 
 }
 var bindTaskEvents= function(taskListItem, checkBoxEventHandler){
+   var checkBox=taskListItem.querySelector("input[type=checkbox]");
+   var editButton=taskListItem.querySelector("button.edit");
+   var deleteButton=taskListItem.querySelector("button.delete");
 
+   checkBox.onchange =checkBoxEventHandler;
+   editButton.onclick =editTask;
+   deleteButton.onclick =deleteTask;
 }
+
 
 
 addButton.onclick = addTask;
@@ -41,5 +50,5 @@ bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
 }
 
 for (var i=0; i<completedTasksHolder.children.length; i++){
-  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete); 
+  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
