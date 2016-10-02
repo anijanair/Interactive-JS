93682366965500b6e2a6fa835e4c8eda7c83a28f
Anija <anija.nair@yahoo.com>
2016-10-01 23:33:44 -0400

    created and appended elements for interactivity

--- Todo App/js/app.js
+++ Todo App/js/app.js
@@ -1,34 +1,61 @@
 //Problem: user interaction doesn't give any results
 //Solution: Add interactivity using javascript
-var taskInput =document.getElementById("new-task"); //new task
-var addButton =document.getElementsByTagName("button")[0]; //first button
-var incompleteTasksHolder =document.getElementById("incomplete-tasks");
-var completedTasksHolder =document.getElementById("completed-tasks");
+var taskInput = document.getElementById("new-task"); //new task
+var addButton = document.getElementsByTagName("button")[0]; //first button
+var incompleteTasksHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder = document.getElementById("completed-tasks");
 
+var createNewTaskElement = function(taskString){
+  var listItem=document.createElement("li");
+  var checkBox = document.createElement("checkbox");
+  var label = document.createElement("label");
+  var editInput = document.createElement("input");
+  var editButton = document.createElement("button");
+  var deleteButton = document.createElement("button");
+
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
+  listItem.appendChild(editInput);
+  listItem.appendChild(editButton);
+  listItem.appendChild(deleteButton);
+  return listItem;
+}
 //add new task
 var addTask = function(){
 console.log("add texts..");
+  var listItem = createNewTaskElement("some new task");
+  incompleteTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
 }
 
 //edit existing task
 var editTask = function(){
-console.log( "edit aaa");
+console.log( "edit text");
 }
 
 //delete an existing task
 var deleteTask = function(){
   console.log("delete ... text..");
-
+  var listItem = this.parentNode;//'this' here is the deleteButton
+  var ul = listItem.parentNode;
+  ul.removeChild(listItem);
 }
 
 //mark a task as completed
-var taskCompleted = function(){console.log("finish text..");
+var taskCompleted = function(){
+  console.log("finished task..");
+  var listItem = this.parentNode// 'this' here is the checkBox
+  completedTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskIncomplete);
 
 }
 
 //mark a task as incomplete
 var taskIncomplete = function(){
   console.log("incomplete text..");
+  var listItem = this.parentNode// 'this' here is the checkBox
+  incompleteTasksHolder.appendChild(listItem);
+
 
 }
 var bindTaskEvents= function(taskListItem, checkBoxEventHandler){
